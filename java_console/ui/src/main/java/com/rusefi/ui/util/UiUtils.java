e7d74cbaf37bd8f87842b7f71a28e924500d121e
Matthew Kennedy <matthewkennedy@outlook.com>
2023-04-12 21:51:26 -0700

    why does autoupdate util have nothing to do with autoupdate

--- java_console/ui/src/main/java/com/rusefi/ui/util/UiUtils.java
+++ java_console/ui/src/main/java/com/rusefi/ui/util/UiUtils.java
@@ -1,7 +1,7 @@
 package com.rusefi.ui.util;
 
-import com.rusefi.core.ui.AutoupdateUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -18,6 +18,11 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLStreamHandlerFactory;
+
 import static com.rusefi.ui.util.LocalizedMessages.CLEAR;
 import static com.rusefi.ui.util.LocalizedMessages.PAUSE;
 import static com.rusefi.ui.util.LocalizedMessages.RESUME;
@@ -28,6 +33,10 @@ import static com.rusefi.ui.util.LocalizedMessages.RESUME;
  */
 public class UiUtils {
     private static final String SAVE_IMAGE = "save image";
+
+    // todo: figure out a better way to work with absolute path
+    private static final String APPICON = "/appicon.png";
+
 
     public static void saveImageWithPrompt(String fileName, Component parentForDialog, Component content) {
         JFileChooser fc = getFileChooser(new FileNameExtensionFilter("PNG files", "png"));
@@ -154,7 +163,7 @@ public class UiUtils {
     }
 
     public static JComponent wrap(JComponent component) {
-        AutoupdateUtil.assertAwtThread();
+        assertAwtThread();
         JPanel result = new JPanel();
         result.add(component);
         return result;
@@ -226,5 +235,60 @@ public class UiUtils {
             }
         });
         return copy;
+    }
+
+    private static class DynamicForResourcesURLClassLoader extends URLClassLoader {
+        public DynamicForResourcesURLClassLoader( URL[] urls, ClassLoader parent ) { super( urls, parent ); }
+        public DynamicForResourcesURLClassLoader( URL[] urls ) { super( urls ); }
+        public DynamicForResourcesURLClassLoader( URL[] urls, ClassLoader parent, URLStreamHandlerFactory factory ) { super( urls, parent, factory ); }
+
+        @Override
+        public void addURL( URL url ) {
+            super.addURL( url );
+        }
+
+        /**
+         * Let's here emulate Class.getResource() logic
+         * @param name resource name
+         * @return resource url
+         */
+        @Nullable
+        @Override
+        public URL getResource( String name ) {
+            if ( name.startsWith( "/" ) )
+                name = name.substring( 1 );
+            return super.getResource( name );
+        }
+    }
+
+    private static final DynamicForResourcesURLClassLoader dynamicResourcesLoader = new DynamicForResourcesURLClassLoader( new URL[ 0 ], UiUtils.class.getClassLoader() );
+
+    @NotNull
+    public static URLClassLoader getClassLoaderByJar(String jar) throws MalformedURLException {
+        final URL jarURL = new File( jar ).toURI().toURL();
+        dynamicResourcesLoader.addURL( jarURL );
+        return new URLClassLoader(
+                new URL[]{ new File( jar ).toURI().toURL() },
+                dynamicResourcesLoader
+        );
+    }
+
+    public static void setAppIcon(JFrame frame) {
+        ImageIcon icon = loadIcon(APPICON);
+        if (icon != null)
+            frame.setIconImage(icon.getImage());
+    }
+
+    public static ImageIcon loadIcon( String strPath ) {
+        URL imgURL = dynamicResourcesLoader.getResource( strPath );
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            imgURL = dynamicResourcesLoader.getResource("/com/rusefi/" + strPath);
+            if (imgURL != null) {
+                return new ImageIcon(imgURL);
+            }
+            return null;
+        }
     }
 }
