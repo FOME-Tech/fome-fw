2bf991fd13a143bf486bb6a177c18b54692229f4
Matthew Kennedy <matthewkennedy@outlook.com>
2023-07-20 16:14:10 -0700

    stale comment

--- java_tools/configuration_definition/src/test/java/com/rusefi/test/newParse/NewParseHelper.java
+++ java_tools/configuration_definition/src/test/java/com/rusefi/test/newParse/NewParseHelper.java
@@ -17,7 +17,6 @@ public class NewParseHelper {
         return parseState;
     }
 
-    // TODO: We have to move either forward or backwards with newparse #4441
     public static String parseToTs(String input) throws IOException {
         ParseState state = parse(input);
 
@@ -47,7 +46,6 @@ public class NewParseHelper {
         return baos.toString(utf8);
     }
 
-    // TODO: We have to move either forward or backwards with newparse #4441
     public static String parseToC(String input) throws IOException {
         ParseState state = parse(input);
 
