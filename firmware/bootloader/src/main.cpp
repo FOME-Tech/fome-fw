b0e81a055f1f94c7c6a0b69a5a98a1e76562a394
Matthew Kennedy <matthewkennedy@outlook.com>
2023-08-25 23:04:36 -0700

    USB enumerates!

--- firmware/bootloader/src/main.cpp
+++ firmware/bootloader/src/main.cpp
@@ -9,13 +9,22 @@ int main(void) {
 
 	baseMCUInit();
 
-	// set base pin configuration based on the board
-	setDefaultBasePins();
-
 	// Set up USB
 	usb_serial_start();
 
 	while (true) {
 		chThdSleepMilliseconds(1);
 	}
+}
+
+// very basic version, supports on chip pins only (really only used for USB)
+void efiSetPadMode(const char* msg, brain_pin_e brainPin, iomode_t mode) {
+	ioportid_t port = getHwPort(msg, brainPin);
+	ioportmask_t pin = getHwPin(msg, brainPin);
+	/* paranoid */
+	if (port == GPIO_NULL) {
+		return;
+	}
+
+	palSetPadMode(port, pin, mode);
 }
