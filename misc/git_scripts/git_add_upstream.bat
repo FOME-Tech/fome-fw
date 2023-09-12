106abf9b0dc57ae61362d48627779ac6dd50726d
mi-hol <22799428+mi-hol@users.noreply.github.com>
2023-09-12 09:34:04 +0200

    Update misc/git_scripts/git_add_upstream.bat
    Co-authored-by: Nathan Schulte <8540239+nmschulte@users.noreply.github.com>

--- misc/git_scripts/git_add_upstream.bat
+++ misc/git_scripts/git_add_upstream.bat
@@ -3,5 +3,6 @@ git remote add upstream https://github.com/FOME-Tech/fome-fw.git
 
 rem The heros, in alpabetical order
 git remote add mck1117      https://github.com/mck1117/fome-fw 
+git remote add nmschulte    https://github.com/nmschulte/fome-fw.git
 
 git remote -v
