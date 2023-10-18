5baca3c23c5e1ed4bff0827dc06f64aed9d50ce0
Benas Brazdziunas <b.brazdziunas@gmail.com>
2023-10-18 10:29:08 +0100

    Add core48 and remove 48way

---
+++ firmware/config/boards/core48/board.mk
@@ -0,0 +1,10 @@
+# List of all the board related files.
+BOARDCPPSRC =  $(PROJECT_DIR)/config/boards/core8/board_configuration.cpp
+
+BOARDINC = $(PROJECT_DIR)/config/boards/core8
+
+# Override DEFAULT_ENGINE_TYPE
+DDEFS += -DSHORT_BOARD_NAME=core8
+DDEFS += -DFIRMWARE_ID=\"core8\"
+DDEFS += -DDEFAULT_ENGINE_TYPE=engine_type_e::MINIMAL_PINS
+DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE -DSTM32_ADC_USE_ADC3=TRUE
