8d18fa1fdd8041ce156629ed619bb491e7949274
Benas Brazdziunas <b.brazdziunas@gmail.com>
2023-10-25 09:08:27 +0100

    engine_type_e:: removed from board.mk

--- firmware/config/boards/core48/board.mk
+++ firmware/config/boards/core48/board.mk
@@ -1,10 +1,8 @@
 # List of all the board related files.
-BOARDCPPSRC =  $(PROJECT_DIR)/config/boards/core48/board_configuration.cpp
-
-BOARDINC = $(PROJECT_DIR)/config/boards/core48
+BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
 
 # Override DEFAULT_ENGINE_TYPE
 DDEFS += -DSHORT_BOARD_NAME=core48
 DDEFS += -DFIRMWARE_ID=\"core48\"
-DDEFS += -DDEFAULT_ENGINE_TYPE=engine_type_e::MINIMAL_PINS
+DDEFS += -DDEFAULT_ENGINE_TYPE=MINIMAL_PINS
 DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE -DSTM32_ADC_USE_ADC3=TRUE
