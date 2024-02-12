45d95494ba61625cf3fe3c5dfe4b72322e4930c7
Matthew Kennedy <matthewkennedy@outlook.com>
2024-02-12 00:53:23 -0800

    select darwin vs. linux properly

--- firmware/use_arm_gcc.mk
+++ firmware/use_arm_gcc.mk
@@ -1,10 +1,14 @@
 # Try to find arm-none-eabi-gcc 
 ifndef $(shell command -v arm-none-eabi-gcc 2> /dev/null)
 # No compiler found, use the one from submodule
-	TRGT = $(PROJECT_DIR)/ext/compiler-experiment/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-
+	UNAME_S := $(shell uname -s)
+	ifeq ($(UNAME_S),Darwin)
+		TRGT = $(PROJECT_DIR)/ext/compiler-experiment/arm-gnu-toolchain-11.3.rel1-darwin-x86_64-arm-none-eabi/bin/arm-none-eabi-
+	else
+		TRGT = $(PROJECT_DIR)/ext/compiler-experiment/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-
+	endif
 else
 # Use the compiler already in $PATH
-# TODO: select correct compiler per-platform (win/mac/lin/etc)
 	TRGT = arm-none-eabi-
 endif
 
