6014e2753400006fba2f224f413883ce41379263
Matthew Kennedy <matthewkennedy@outlook.com>
2024-02-12 09:40:49 +0000

    simplify and print a little more info

--- firmware/use_arm_gcc.mk
+++ firmware/use_arm_gcc.mk
@@ -1,8 +1,11 @@
 UNAME_S := $(shell uname -s)
 ifeq ($(UNAME_S),Darwin)
-	TRGT = $(PROJECT_DIR)/ext/compiler-experiment/arm-gnu-toolchain-11.3.rel1-darwin-x86_64-arm-none-eabi/bin/arm-none-eabi-
+	COMPILER_PLATFORM = arm-gnu-toolchain-11.3.rel1-darwin-x86_64-arm-none-eabi
 else
-	TRGT = $(PROJECT_DIR)/ext/compiler-experiment/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-
+	COMPILER_PLATFORM = arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi
 endif
 
-$(info TRGT:          $(TRGT))
+TRGT = $(PROJECT_DIR)/ext/compiler-experiment/$(COMPILER_PLATFORM)/bin/arm-none-eabi-
+
+$(info COMPILER_PLATFORM: $(COMPILER_PLATFORM))
+$(info TRGT:              $(TRGT))
