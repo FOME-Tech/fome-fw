ef83f7dc5c5be64ea294074f1abda3dabc4be5b8
Matthew Kennedy <matthewkennedy@outlook.com>
2024-02-12 08:47:00 +0000

    add todo

--- firmware/use_arm_gcc.mk
+++ firmware/use_arm_gcc.mk
@@ -4,6 +4,7 @@ ifndef $(shell command -v arm-none-eabi-gcc 2> /dev/null)
 	TRGT = $(PROJECT_DIR)/ext/compiler-experiment/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-
 else
 # Use the compiler already in $PATH
+# TODO: select correct compiler per-platform (win/mac/lin/etc)
 	TRGT = arm-none-eabi-
 endif
 
