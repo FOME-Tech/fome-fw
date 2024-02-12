18028cf65ce511254cc199b3684d3c9a31b0b3e8
Matthew Kennedy <matthewkennedy@outlook.com>
2024-02-12 09:31:45 +0000

    don't ever bother trying to use the installed compiler

--- firmware/use_arm_gcc.mk
+++ firmware/use_arm_gcc.mk
@@ -1,15 +1,8 @@
-# Try to find arm-none-eabi-gcc 
-ifndef $(shell command -v arm-none-eabi-gcc 2> /dev/null)
-# No compiler found, use the one from submodule
-	UNAME_S := $(shell uname -s)
-	ifeq ($(UNAME_S),Darwin)
-		TRGT = $(PROJECT_DIR)/ext/compiler-experiment/arm-gnu-toolchain-11.3.rel1-darwin-x86_64-arm-none-eabi/bin/arm-none-eabi-
-	else
-		TRGT = $(PROJECT_DIR)/ext/compiler-experiment/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-
-	endif
+UNAME_S := $(shell uname -s)
+ifeq ($(UNAME_S),Darwin)
+	TRGT = $(PROJECT_DIR)/ext/compiler-experiment/arm-gnu-toolchain-11.3.rel1-darwin-x86_64-arm-none-eabi/bin/arm-none-eabi-
 else
-# Use the compiler already in $PATH
-	TRGT = arm-none-eabi-
+	TRGT = $(PROJECT_DIR)/ext/compiler-experiment/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-
 endif
 
 $(info TRGT:          $(TRGT))
