6a27d5a3d882f86ce4b750112e34e7c9acad6235
mi-hol <22799428+mi-hol@users.noreply.github.com>
2024-02-14 00:04:12 +0100

    Create benelli.h

---
+++ firmware/config/engines/benelli.h
@@ -0,0 +1,15 @@
+/*
+ * @file benelli.h
+ *
+ * @date Feb 13, 2024
+ * @author Michael Holzer, (c) 2024
+ */
+
+#pragma once
+
+#include "engine_configuration.h"
+
+void setBenelliTre();
+
+void set_Mre_Benelli_3_cyl_900ccm();
+void set_Mre_Benelli_3_cyl_1130ccm();
