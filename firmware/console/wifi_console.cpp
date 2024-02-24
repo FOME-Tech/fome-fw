5347c17097e25377247db67b831d6d2c9923f5a4
Matthew Kennedy <matthewkennedy@outlook.com>
2024-02-23 22:15:12 -0800

    more wifi plumbing

--- firmware/console/wifi_console.cpp
+++ firmware/console/wifi_console.cpp
@@ -57,10 +57,51 @@ public:
 
 static WifiChannel wifiChannel;
 
-struct WifiThread : public TunerstudioThread {
-	WifiThread() : TunerstudioThread("WiFi Console") { }
+class WifiHelperThread : public ThreadController<256> {
+public:
+	WifiHelperThread() : ThreadController("WifiPump", LWIP_THREAD_PRIORITY) {}
+	void ThreadTask() override {
+		while (true)
+		{
+			m2m_wifi_handle_events(nullptr);
+		}
+	}
+};
+
+static WifiHelperThread wifiHelper;
+
+static tstrWifiInitParam param;
+
+static tstrM2MAPConfig apConfig;
+
+struct WifiConsoleThread : public TunerstudioThread {
+	WifiConsoleThread() : TunerstudioThread("WiFi Console") { }
 
 	TsChannelBase* setupChannel() override {
+		if (M2M_SUCCESS != m2m_wifi_init(&param)) {
+			return nullptr;
+		}
+
+		strcpy(apConfig.au8SSID, "WINC_SSID");
+		apConfig.u8ListenChannel 	= 1;
+		apConfig.u8SecType			= M2M_WIFI_SEC_OPEN;
+		apConfig.u8SsidHide			= 0;
+
+		// IP Address
+		apConfig.au8DHCPServerIP[0]	= 192;
+		apConfig.au8DHCPServerIP[1]	= 168;
+		apConfig.au8DHCPServerIP[2]	= 1;
+		apConfig.au8DHCPServerIP[3]	= 1;
+
+		// Trigger AP
+		if (M2M_SUCCESS != m2m_wifi_enable_ap(&apConfig)) {
+			return nullptr;
+		}
+
+		// Start the helper thread
+		wifiHelper.start();
+
+		// Start listening on the socket
 		sockaddr_in address;
 		address.sin_family = AF_INET;
 		address.sin_port = _htons(29000);
@@ -76,31 +117,9 @@ struct WifiThread : public TunerstudioThread {
 	}
 };
 
-static WifiThread wifiThread;
-
-static tstrWifiInitParam param;
-
-static tstrM2MAPConfig apConfig;
+static WifiConsoleThread wifiThread;
 
 void startWifiConsole() {
-	m2m_wifi_init(&param);
-
-	strcpy(apConfig.au8SSID, "WINC_SSID");
-	apConfig.u8ListenChannel 	= 1;
-	apConfig.u8SecType			= M2M_WIFI_SEC_OPEN;
-	apConfig.u8SsidHide			= 0;
-	
-	// IP Address
-	apConfig.au8DHCPServerIP[0]	= 192;
-	apConfig.au8DHCPServerIP[1]	= 168;
-	apConfig.au8DHCPServerIP[2]	= 1;
-	apConfig.au8DHCPServerIP[3]	= 1;
-	
-	// Trigger AP
-	m2m_wifi_enable_ap(&apConfig);
-
-	m2m_wifi_handle_events(nullptr);
-
 	wifiThread.start();
 }
 
--- firmware/hw_layer/atwinc1500/wifi_bsp.cpp
+++ firmware/hw_layer/atwinc1500/wifi_bsp.cpp
@@ -7,16 +7,16 @@
 
 #include "bus_wrapper/include/nm_bus_wrapper.h"
 
+void nm_bsp_sleep(uint32 u32TimeMsec) {
+	chThdSleepMilliseconds(u32TimeMsec);
+}
+
 static tpfNmBspIsr gpfIsr = nullptr;
 
 static void isrAdapter(void*, efitick_t) {
 	if (gpfIsr) {
 		gpfIsr();
 	}
-}
-
-void nm_bsp_sleep(uint32 u32TimeMsec) {
-	chThdSleepMilliseconds(u32TimeMsec);
 }
 
 void nm_bsp_interrupt_ctrl(uint8 u8Enable) {
@@ -27,8 +27,7 @@ void nm_bsp_interrupt_ctrl(uint8 u8Enable) {
 	}
 }
 
-void nm_bsp_register_isr(tpfNmBspIsr pfIsr)
-{
+void nm_bsp_register_isr(tpfNmBspIsr pfIsr) {
 	gpfIsr = pfIsr;
 
 	nm_bsp_interrupt_ctrl(1);
@@ -38,22 +37,21 @@ static SPIDriver* wifiSpi = &SPID1;
 
 #define NM_BUS_MAX_TRX_SZ	512
 
-tstrNmBusCapabilities egstrNmBusCapabilities =
-{
-	NM_BUS_MAX_TRX_SZ
+tstrNmBusCapabilities egstrNmBusCapabilities = { .u16MaxTrxSz = 512 };
+
+static SPIConfig wifi_spicfg = {
+		.circular = false,
+		.end_cb = NULL,
+		.ssport = NULL,
+		.sspad = 0,
+		.cr1 = SPI_BaudRatePrescaler_2,
+		.cr2 = 0
 };
 
-sint8 nm_spi_rw(uint8* pu8Mosi, uint8* pu8Miso, uint16 u16Sz) {
-	spiExchange(wifiSpi, u16Sz, pu8Mosi, pu8Miso);
+sint8 nm_bus_init(void*) {
+	spiStart(wifiSpi, &wifi_spicfg);
 
 	return M2M_SUCCESS;
-}
-
-sint8 nm_bus_init(void*) {
-	// TODO!
-	spiStart(wifiSpi, nullptr);
-
-	return 0;
 }
 
 sint8 nm_bus_deinit(void) {
@@ -64,5 +62,21 @@ sint8 nm_bus_deinit(void) {
 
 sint8 nm_bus_speed(uint8 level) {
 	// Do we even need to do anything here?
+	return M2M_SUCCESS;
+}
+
+sint8 nm_spi_rw(uint8* pu8Mosi, uint8* pu8Miso, uint16 u16Sz) {
+	spiAcquireBus(wifiSpi);
+
+	if (u16Sz < 16) {
+		for (size_t i = 0; i < u16Sz; i++) {
+			pu8Miso[i] = spiPolledExchange(wifiSpi, pu8Mosi[i]);
+		}
+	} else {
+		spiExchange(wifiSpi, u16Sz, pu8Mosi, pu8Miso);
+	}
+
+	spiReleaseBus(wifiSpi);
+
 	return M2M_SUCCESS;
 }
