76b8b7d35d4367df21ff7d7093b3e319da210d30
Matthew Kennedy <matthewkennedy@outlook.com>
2024-03-21 15:40:15 -0700

    use correct time conversion function

--- firmware/init/sensor/init_egt.cpp
+++ firmware/init/sensor/init_egt.cpp
@@ -3,14 +3,14 @@
 #include "max31855.h"
 
 static StoredValueSensor egts[] = {
-	{ SensorType::Egt1, TIME_MS2I(1000) },
-	{ SensorType::Egt2, TIME_MS2I(1000) },
-	{ SensorType::Egt3, TIME_MS2I(1000) },
-	{ SensorType::Egt4, TIME_MS2I(1000) },
-	{ SensorType::Egt5, TIME_MS2I(1000) },
-	{ SensorType::Egt6, TIME_MS2I(1000) },
-	{ SensorType::Egt7, TIME_MS2I(1000) },
-	{ SensorType::Egt8, TIME_MS2I(1000) },
+	{ SensorType::Egt1, MS2NT(1000) },
+	{ SensorType::Egt2, MS2NT(1000) },
+	{ SensorType::Egt3, MS2NT(1000) },
+	{ SensorType::Egt4, MS2NT(1000) },
+	{ SensorType::Egt5, MS2NT(1000) },
+	{ SensorType::Egt6, MS2NT(1000) },
+	{ SensorType::Egt7, MS2NT(1000) },
+	{ SensorType::Egt8, MS2NT(1000) },
 };
 
 void initEgt() {
