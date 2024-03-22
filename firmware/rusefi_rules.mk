6db0f1ac2d8d7546b4a22f370e088ce50a38f54d
Andrey <rusefillc@gmail.com>
2024-03-22 08:42:49 -0400

    -Werror=stringop-overflow

--- firmware/rusefi_rules.mk
+++ firmware/rusefi_rules.mk
@@ -7,4 +7,4 @@ RUSEFI_OPT += -Wno-error=unused-function
 RUSEFI_OPT += -Wno-error=sign-compare
 RUSEFI_OPT += -Wno-error=unused-parameter
 RUSEFI_OPT += -Wno-error=undef
-RUSEFI_OPT += -Wno-error=stringop-overflow
+
