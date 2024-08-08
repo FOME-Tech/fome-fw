7f1f1bb2e96540de784c66ca078be5085d7b50b5
firebat45 <firebat45@gmail.com>
2024-08-07 20:00:33 -0600

    update with shoefi entry

--- firmware/gen_config.sh
+++ firmware/gen_config.sh
@@ -47,6 +47,7 @@ for BOARD in \
    "config/boards/f429-discovery f429-discovery" \
    "config/boards/atlas atlas"\
    "config/boards/tdg-pdm8 tdg-pdm8"\
+   "config/boards/shoefi shoefi"\
    ; do
  BOARD_NAME=$(echo "$BOARD" | cut -d " " -f 1)
  BOARD_SHORT_NAME=$(echo "$BOARD" | cut -d " " -f 2)
