9f6bc09fd6dbccfef116525108b9dc8fa9775ff7
firebat45 <firebat45@gmail.com>
2024-09-01 19:54:46 -0600

    test

--- firmware/config/boards/shoefi/compile_shoefi.sh
+++ firmware/config/boards/shoefi/compile_shoefi.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# STM32F4...
-
-SCRIPT_NAME="compile_nucleo_f429.sh"
-echo "Entering $SCRIPT_NAME"
-
 export USE_OPENBLT=yes
 
-bash ../common_make.sh nucleo_f429 ARCH_STM32F4
+bash ../common_make.sh shoefi ARCH_STM32F4
