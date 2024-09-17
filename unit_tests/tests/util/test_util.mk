21b6ee264393fb6a6386fb0253fdc5a716171aec
Matthew Kennedy <matthewkennedy@outlook.com>
2024-09-17 16:39:16 -0700

    simpler ETB jam detection #489 rather than try to integrate error over time, simply fault if there's been too much error for too long. Up to +-X error is allowed for any period of time, more is allowed for Y seconds.

--- unit_tests/tests/util/test_util.mk
+++ unit_tests/tests/util/test_util.mk
@@ -1,7 +1,6 @@
 
 
 CPPSRC += 	$(PROJECT_DIR)/../unit_tests/tests/util/test_buffered_writer.cpp \
-	$(PROJECT_DIR)/../unit_tests/tests/util/test_error_accumulator.cpp \
 	$(PROJECT_DIR)/../unit_tests/tests/util/test_exp_average.cpp \
 	$(PROJECT_DIR)/../unit_tests/tests/util/test_hash.cpp \
 	
