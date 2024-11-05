95be13cd0b920321631aab60904aa78173681392
Matthew Kennedy <matthewkennedy@outlook.com>
2024-11-05 22:05:20 +0000

    add dosfstools to deps

--- firmware/setup_linux_environment.sh
+++ firmware/setup_linux_environment.sh
@@ -23,7 +23,7 @@ git submodule update --init
 sudo apt-get update
 
 # install dependencies
-sudo apt-get install -y build-essential gcc gdb gcc-multilib g++-multilib make openjdk-8-jdk-headless mtools zip xxd libncurses5 libncursesw5
+sudo apt-get install -y build-essential gcc gdb gcc-multilib g++-multilib make openjdk-8-jdk-headless mtools zip xxd libncurses5 libncursesw5 dosfstools
 
 # Allow the current user to use serial ports
 sudo usermod -a -G dialout $USER
