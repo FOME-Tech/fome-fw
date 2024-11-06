8c4a140141dbc45eec8b282e6cca603eeb7c8a91
Matthew Kennedy <matthewkennedy@outlook.com>
2024-11-06 00:41:40 +0000

    Revert "unused script"
    This reverts commit 5619cc67d5849e25479ee9c92b7265ec0f6775fc.

---
+++ firmware/gen_default_everything.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# is order of operations important here or not really?
+
+echo "gen_live_documentation.sh as part of everything"
+./gen_live_documentation.sh
+
+echo "gen_config_default.sh as part of everything"
+./gen_config_default.sh
