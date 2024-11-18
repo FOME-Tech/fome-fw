bf4bff253e4d9c317ed45e1622661a11739977ac
Matthew Kennedy <matthewkennedy@outlook.com>
2024-11-18 02:32:34 -0800

    atlas fiddling

--- firmware/config/boards/atlas/board.mk
+++ firmware/config/boards/atlas/board.mk
@@ -8,8 +8,11 @@ DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::F0
 DDEFS += -DFIRMWARE_ID=\"atlas\"
 
 # This stuff doesn't work on H7 yet
-# DDEFS += -DSTM32_ADC_USE_ADC3=TRUE
-# DEFS += -DEFI_SOFTWARE_KNOCK=TRUE
+# This board has trigger scope hardware!
+# DDEFS += -DTRIGGER_SCOPE
+# DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE -DSTM32_ADC_USE_ADC3=TRUE
+
+DDEFS += -DEFI_SDC_DEVICE=SDCD1
 
 # We are running on Atlas hardware!
 DDEFS += -DHW_ATLAS=1
