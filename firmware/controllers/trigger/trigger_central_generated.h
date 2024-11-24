0bab9e065c36cfeff2b9f68426bf1a7c4c157222
GitHub gen-configs Action <action@github.com>
2024-11-24 07:09:48 +0000

    Auto-generated configs and docs

--- firmware/controllers/trigger/trigger_central_generated.h
+++ firmware/controllers/trigger/trigger_central_generated.h
@@ -3,41 +3,39 @@
 struct trigger_central_s {
 	// Hardware events since boot
 	// offset 0
-	uint16_t hwEventCounters[4];
+	uint16_t triggerEventCounter[4];
 	// offset 8
-	uint16_t vvtCamCounter = (uint16_t)0;
-	// offset 10
-	uint8_t alignmentFill_at_10[2];
-	// offset 12
-	float mapVvt_MAP_AT_SPECIAL_POINT = (float)0;
+	uint16_t vvtCamCounter[4];
 	// offset 16
-	float mapVvt_MAP_AT_DIFF = (float)0;
+	float mapVvt_MAP_AT_SPECIAL_POINT = (float)0;
 	// offset 20
+	float mapVvt_MAP_AT_DIFF = (float)0;
+	// offset 24
 	uint8_t mapVvt_MAP_AT_CYCLE_COUNT = (uint8_t)0;
-	// offset 21
+	// offset 25
 	uint8_t mapVvt_map_peak = (uint8_t)0;
-	// offset 22
-	uint8_t alignmentFill_at_22[2];
+	// offset 26
+	uint8_t alignmentFill_at_26[2];
 	// Engine Phase
 	// deg
-	// offset 24
+	// offset 28
 	float currentEngineDecodedPhase = (float)0;
 	// deg
-	// offset 28
-	float triggerToothAngleError = (float)0;
 	// offset 32
+	float triggerToothAngleError = (float)0;
+	// offset 36
 	uint8_t triggerIgnoredToothCount = (uint8_t)0;
-	// offset 33
-	uint8_t alignmentFill_at_33[3];
+	// offset 37
+	uint8_t alignmentFill_at_37[3];
 };
-static_assert(sizeof(trigger_central_s) == 36);
-static_assert(offsetof(trigger_central_s, hwEventCounters) == 0);
+static_assert(sizeof(trigger_central_s) == 40);
+static_assert(offsetof(trigger_central_s, triggerEventCounter) == 0);
 static_assert(offsetof(trigger_central_s, vvtCamCounter) == 8);
-static_assert(offsetof(trigger_central_s, mapVvt_MAP_AT_SPECIAL_POINT) == 12);
-static_assert(offsetof(trigger_central_s, mapVvt_MAP_AT_DIFF) == 16);
-static_assert(offsetof(trigger_central_s, mapVvt_MAP_AT_CYCLE_COUNT) == 20);
-static_assert(offsetof(trigger_central_s, mapVvt_map_peak) == 21);
-static_assert(offsetof(trigger_central_s, currentEngineDecodedPhase) == 24);
-static_assert(offsetof(trigger_central_s, triggerToothAngleError) == 28);
-static_assert(offsetof(trigger_central_s, triggerIgnoredToothCount) == 32);
+static_assert(offsetof(trigger_central_s, mapVvt_MAP_AT_SPECIAL_POINT) == 16);
+static_assert(offsetof(trigger_central_s, mapVvt_MAP_AT_DIFF) == 20);
+static_assert(offsetof(trigger_central_s, mapVvt_MAP_AT_CYCLE_COUNT) == 24);
+static_assert(offsetof(trigger_central_s, mapVvt_map_peak) == 25);
+static_assert(offsetof(trigger_central_s, currentEngineDecodedPhase) == 28);
+static_assert(offsetof(trigger_central_s, triggerToothAngleError) == 32);
+static_assert(offsetof(trigger_central_s, triggerIgnoredToothCount) == 36);
 
