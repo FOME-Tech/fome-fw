6a7a45c0cb7b6d7be1287c908c856cff109030c3
Matthew Kennedy <matthewkennedy@outlook.com>
2024-12-24 17:45:46 -0800

    H743 nucleo build

--- firmware/config/boards/nucleo_h743/board.mk
+++ firmware/config/boards/nucleo_h743/board.mk
@@ -3,10 +3,13 @@
 BOARDCPPSRC = $(BOARD_DIR)/board_configuration.cpp
 
 DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::B14
+DDEFS += -DSTM32_ADC_USE_ADC3=TRUE
 
 # Enable ethernet
-LWIP = yes
-include $(PROJECT_DIR)/controllers/modules/ethernet_console/ethernet_console.mk
+LWIP = no
+#include $(PROJECT_DIR)/controllers/modules/ethernet_console/ethernet_console.mk
+
+SHORT_BOARD_NAME = stm32h743_nucleo
 
 DDEFS += -DFIRMWARE_ID=\"nucleo_h743\"
 DDEFS += -DDEFAULT_ENGINE_TYPE=MINIMAL_PINS
