8cced628d73a4ddefe64748b11a1bee2bfd6c8f4
Matthew Kennedy <matthewkennedy@outlook.com>
2024-12-26 00:51:41 -0800

    fix can verbose TX period

--- firmware/controllers/can/can_tx.cpp
+++ firmware/controllers/can/can_tx.cpp
@@ -25,18 +25,28 @@ CanWrite::CanWrite()
 {
 }
 
+static CI roundTxPeriodToCycle(uint16_t period) {
+	if (period < 10) return CI::_5ms;
+	else if (period < 20) return CI::_10ms;
+	else if (period < 50) return CI::_20ms;
+	else if (period < 100) return CI::_50ms;
+	else if (period < 200) return CI::_100ms;
+	else if (period < 250) return CI::_200ms;
+	else if (period < 500) return CI::_250ms;
+	else if (period < 1000) return CI::_500ms;
+	else return CI::_1000ms;
+}
+
 void CanWrite::PeriodicTask(efitick_t) {
 	static uint16_t cycleCount = 0;
 	CanCycle cycle(cycleCount);
 
 	//in case we have Verbose Can enabled, we should keep user configured period
 	if (engineConfiguration->enableVerboseCanTx && !engine->pauseCANdueToSerial) {
-		uint16_t cycleCountsPeriodMs = cycleCount * CAN_CYCLE_PERIOD;
-		if (0 != engineConfiguration->canSleepPeriodMs) {
-			if (cycleCountsPeriodMs % engineConfiguration->canSleepPeriodMs) {
-				void sendCanVerbose();
-				sendCanVerbose();
-			}
+		auto roundedInterval = roundTxPeriodToCycle(engineConfiguration->canSleepPeriodMs);
+		if (cycle.isInterval(roundedInterval)) {
+			void sendCanVerbose();
+			sendCanVerbose();
 		}
 	}
 
