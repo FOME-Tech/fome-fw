f22d5c815552682b73a7bfe5301e6fa2176bd534
Matthew Kennedy <matthewkennedy@outlook.com>
2025-02-14 13:01:18 -0800

    calculate spark timing last minute rather than storing it

--- firmware/controllers/algo/event_registry.h
+++ firmware/controllers/algo/event_registry.h
@@ -44,10 +44,6 @@ public:
 	// Track whether coil charge was intentionally skipped (spark limiter)
 	bool wasSparkLimited = false;
 
-	/**
-	 * Desired timing advance
-	 */
-	angle_t sparkAngle = NAN;
 	floatms_t sparkDwell = 0;
 
 	// this timer allows us to measure actual dwell time
