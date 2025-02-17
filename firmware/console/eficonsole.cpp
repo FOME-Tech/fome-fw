ab56e836d4097adea5abbcc349b2f41325079df5
Matthew Kennedy <matthewkennedy@outlook.com>
2025-02-17 01:33:10 -0800

    ok make it non-infinite recursion

--- firmware/console/eficonsole.cpp
+++ firmware/console/eficonsole.cpp
@@ -149,7 +149,13 @@ static void cmd_threads() {
 }
 
 static int fib(int x) {
-	return fib(x - 1) + fib(x - 2);
+	if (x == 0) {
+		return 0;
+	} else if (x == 1) {
+		return 1;
+	} else {
+		return fib(x - 1) + fib(x - 2);
+	}
 }
 
 void initializeConsole() {
