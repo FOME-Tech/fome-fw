a81a4c8a127e34686f0e84b6ffe0916adda96508
Nathan Schulte <8540239+nmschulte@users.noreply.github.com>
2025-03-31 12:00:50 -0500

    stm32f7: fix board.h unification breaks (#577)
    * stm32f7: port EFI_PIN_MODE_DEFAULT/EFI_DR_DEFAULT concept
    
    via 49497dec6d6d3e918c017611ac277c1ea8ba3199
    fixes ea3b865602f59bacfbf536772865329f023c4ebb
    
    * stm32f7: port DEFAULT_GPIO_SPEED concept
    
    via 9e8d72ff1e257d6cc3f790df14982725e6dfba51
    fixes ea3b865602f59bacfbf536772865329f023c4ebb

--- firmware/hw_layer/ports/stm32/stm32f7/board.h
+++ firmware/hw_layer/ports/stm32/stm32f7/board.h
@@ -40,6 +40,17 @@
 #define BOARD_OTG_NOVBUSSENS TRUE
 
 /*
+ * Default to input mode, with internal pulldown resistor enabled.
+ */
+#define EFI_PIN_MODE_DEFAULT PIN_MODE_INPUT
+#ifndef EFI_DR_DEFAULT
+#define EFI_DR_DEFAULT PIN_PUPDR_PULLDOWN
+#endif
+
+// See https://github.com/rusefi/rusefi/issues/397
+#define DEFAULT_GPIO_SPEED PIN_OSPEED_HIGH
+
+/*
  * Ethernet PHY type.
  */
 #define BOARD_PHY_ID                MII_LAN8742A_ID
