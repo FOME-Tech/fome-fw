d5ee90ccd3bc0158e5b8c22706be6775b4450872
Matthew Kennedy <matthewkennedy@outlook.com>
2025-05-03 00:18:42 -0700

    why did we have efiStrlen when it's identical to strlen?

--- firmware/util/efilib.cpp
+++ firmware/util/efilib.cpp
@@ -27,18 +27,11 @@ float efiRound(float value, float precision) {
 	return fixNegativeZero(a * precision);
 }
 
-uint32_t efiStrlen(const char *param) {
-	const char *s;
-	for (s = param; *s; ++s)
-		;
-	return (s - param);
-}
-
 char * efiTrim(char *param) {
 	while (param[0] == ' ') {
 		param++; // that would skip leading spaces
 	}
-	int len = efiStrlen(param);
+	int len = std::strlen(param);
 	while (len > 0 && param[len - 1] == ' ') {
 		param[len - 1] = 0;
 		len--;
@@ -47,8 +40,8 @@ char * efiTrim(char *param) {
 }
 
 bool startsWith(const char *line, const char *prefix) {
-	uint32_t len = efiStrlen(prefix);
-	if (efiStrlen(line) < len) {
+	uint32_t len = std::strlen(prefix);
+	if (std::strlen(line) < len) {
 		return false;
 	}
 	for (uint32_t i = 0; i < len; i++) {
@@ -62,7 +55,7 @@ bool startsWith(const char *line, const char *prefix) {
 int indexOf(const char *string, char c) {
 	// a standard function for this is strnchr?
 	// todo: on the other hand MISRA wants us not to use standard headers
-	int len = efiStrlen(string);
+	int len = std::strlen(string);
 	for (int i = 0; i < len; i++) {
 		if (string[i] == c) {
 			return i;
--- firmware/util/efilib.h
+++ firmware/util/efilib.h
@@ -54,7 +54,6 @@ const char * boolToString(bool value);
 
 char * efiTrim(char *param);
 int mytolower(const char c);
-uint32_t efiStrlen(const char *param);
 int efiPow10(int param);
 bool startsWith(const char *line, const char *prefix);
 int indexOf(const char *string, char ch);
