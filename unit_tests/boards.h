e171948c6d6c27e935e86398d6cd8a82971e4e91
Nathan Schulte <nmschulte@desmas.net>
2025-06-05 14:25:32 -0500

    unit_tests: stub struct port_extctx

--- unit_tests/boards.h
+++ unit_tests/boards.h
@@ -13,3 +13,15 @@
 #define ADC_CHANNEL_VREF 0
 
 int getAdcValue(const char *msg, adc_channel_e channel);
+
+
+// via: ChibiOS/os/common/ports/templates/chcore.h
+/**
+ * @brief   Interrupt saved context.
+ * @details This structure represents the stack frame saved during a
+ *          preemption-capable interrupt handler.
+ * @note    R2 and R13 are not saved because those are assumed to be immutable
+ *          during the system life cycle.
+ */
+struct port_extctx {
+};
