e199b97b7aa1bc5e26a04933bd606dbc6122ffd4
Matthew Kennedy <matthewkennedy@outlook.com>
2025-06-29 23:52:54 -0700

    CAN virtual inputs (#622)
    * mostly implement can virtual inputs
    
    * UI
    
    * hack to make the pins show up
    
    * simulator guards

--- firmware/controllers/can/can_rx.cpp
+++ firmware/controllers/can/can_rx.cpp
@@ -231,6 +231,23 @@ static void processEgtCan(CanBusIndex busIndex, const CANRxFrame& frame) {
 	}
 }
 
+#if EFI_GPIO_HARDWARE
+static void processCanInputPins(CanBusIndex busIndex, const CANRxFrame& frame) {
+	for (size_t i = 0; i < CAN_VIRTUAL_INPUT_PINS_COUNT; i++) {
+		const auto& inputConf = engineConfiguration->canVirtualInputs[i];
+
+		if (CAN_ID(frame) != inputConf.id) {
+			continue;
+		}
+
+		// extract the requested bit
+		bool value = (frame.data8[inputConf.byte] >> inputConf.bitOffset) & 0x01;
+
+		setCanVirtualInput(i, value);
+	}
+}
+#endif
+
 void processCanRxMessage(CanBusIndex busIndex, const CANRxFrame& frame, efitick_t nowNt) {
 	if (engineConfiguration->verboseCan && busIndex == CanBusIndex::Bus0) {
 		printPacket(busIndex, frame);
@@ -252,6 +269,10 @@ void processCanRxMessage(CanBusIndex busIndex, const CANRxFrame& frame, efitick_
 	processLuaCan(busIndex, frame);
 
 	processEgtCan(busIndex, frame);
+
+	#if EFI_GPIO_HARDWARE
+		processCanInputPins(busIndex, frame);
+	#endif
 
 	obdOnCanPacketRx(frame, busIndex);
 
