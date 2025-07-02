55275408acd065e1422c1df7888ba18437c970ed
GitHub gen-configs Action <action@github.com>
2025-07-02 06:57:21 +0000

    Auto-generated configs and docs

--- java_console/models/src/main/java/com/rusefi/config/generated/Fields.java
+++ java_console/models/src/main/java/com/rusefi/config/generated/Fields.java
@@ -947,7 +947,7 @@ public class Fields {
 	public static final int SelectedGear_Neutral = 5;
 	public static final int SelectedGear_Park = 3;
 	public static final int SelectedGear_Reverse = 4;
-	public static final int SIGNATURE_HASH = 903382184;
+	public static final int SIGNATURE_HASH = 1880514686;
 	public static final int spi_device_e_SPI_DEVICE_1 = 1;
 	public static final int spi_device_e_SPI_DEVICE_2 = 2;
 	public static final int spi_device_e_SPI_DEVICE_3 = 3;
@@ -1154,7 +1154,7 @@ public class Fields {
 	public static final int TS_RESPONSE_UNDERRUN = 0x80;
 	public static final int TS_RESPONSE_UNRECOGNIZED_COMMAND = 0x83;
 	public static final char TS_SET_LOGGER_SWITCH = 'l';
-	public static final String TS_SIGNATURE = "rusEFI (FOME) master.2025.07.02.f407-discovery.903382184";
+	public static final String TS_SIGNATURE = "rusEFI (FOME) master.2025.07.02.f407-discovery.1880514686";
 	public static final char TS_SINGLE_WRITE_COMMAND = 'W';
 	public static final int TS_TOTAL_OUTPUT_SIZE = 1308;
 	public static final String TS_TRIGGER_SCOPE_CHANNEL_1_NAME = "Channel 1";
@@ -1967,7 +1967,8 @@ public class Fields {
 	public static final Field TIMING_OFFSET_CYLINDER12 = Field.create("TIMING_OFFSET_CYLINDER12", 1608, FieldType.FLOAT).setBaseOffset(0);
 	public static final Field IDLEPIDACTIVATIONTIME = Field.create("IDLEPIDACTIVATIONTIME", 1612, FieldType.FLOAT).setBaseOffset(0);
 	public static final Field VVTCONTROLMINCLT = Field.create("VVTCONTROLMINCLT", 1616, FieldType.INT8).setScale(1.0).setBaseOffset(0);
-	public static final Field UNUSED1800 = Field.create("UNUSED1800", 1617, FieldType.INT8).setScale(1.0).setBaseOffset(0);
+	public static final Field MINIMUMINJECTIONPULSEWIDTH = Field.create("MINIMUMINJECTIONPULSEWIDTH", 1617, FieldType.INT8).setScale(0.01).setBaseOffset(0);
+	public static final Field UNUSED1800 = Field.create("UNUSED1800", 1618, FieldType.INT8).setScale(1.0).setBaseOffset(0);
 	public static final Field ECUMASTEREGTTOCANBASEID = Field.create("ECUMASTEREGTTOCANBASEID", 1622, FieldType.INT16).setScale(1.0).setBaseOffset(0);
 	public static final Field MINSTEPPERVOLTAGE = Field.create("MINSTEPPERVOLTAGE", 1624, FieldType.INT8).setScale(0.1).setBaseOffset(0);
 	public static final Field STEPPERENABLEPINMODE = Field.create("STEPPERENABLEPINMODE", 1625, FieldType.INT8, pin_output_mode_e).setScale(1.0).setBaseOffset(0);
