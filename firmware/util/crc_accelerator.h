26a48056c578c5146591d20d4f595103ecd8abdf
Matthew Kennedy <matthewkennedy@outlook.com>
2025-07-04 23:36:02 -0700

    static -> inline #631

--- firmware/util/crc_accelerator.h
+++ firmware/util/crc_accelerator.h
@@ -13,7 +13,7 @@ private:
 	uint32_t m_crc = 0;
 };
 
-static uint32_t singleCrc(const void* buf, size_t size) {
+inline uint32_t singleCrc(const void* buf, size_t size) {
 	Crc crc;
 	crc.addData(buf, size);
 	return crc.getCrc();
