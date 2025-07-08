c249796cc57c138893b03ebff808e45da0086a5e
Matthew Kennedy <matthewkennedy@outlook.com>
2025-07-08 01:20:31 -0700

    avoid memset

--- firmware/controllers/trigger/trigger_decoder.cpp
+++ firmware/controllers/trigger/trigger_decoder.cpp
@@ -68,7 +68,7 @@ void TriggerDecoderBase::setTriggerErrorState() {
 }
 
 void TriggerDecoderBase::resetCurrentCycleState() {
-	memset(currentCycle.eventCount, 0, sizeof(currentCycle.eventCount));
+	setArrayValues(currentCycle.eventCount, 0);
 	currentCycle.current_index = 0;
 }
 
