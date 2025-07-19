40950701bd59749245909f0a7019483643040169
Matthew Kennedy <matthewkennedy@outlook.com>
2025-07-19 00:47:30 -0700

    de-crust

--- firmware/controllers/lua/lua_hooks.cpp
+++ firmware/controllers/lua/lua_hooks.cpp
@@ -29,17 +29,6 @@ using namespace luaaa;
 #include "electronic_throttle.h"
 #endif // EFI_PROD_CODE
 
-static int lua_vin(lua_State* l) {
-	auto zeroBasedCharIndex = luaL_checkinteger(l, 1);
-	if (zeroBasedCharIndex < 0 || zeroBasedCharIndex > VIN_NUMBER_SIZE) {
-		lua_pushnil(l);
-	} else {
-		char value = engineConfiguration->vinNumber[zeroBasedCharIndex];
-		lua_pushnumber(l, value);
-	}
-	return 1;
-}
-
 static int lua_readpin(lua_State* l) {
 #if EFI_PROD_CODE
 	auto msg = luaL_checkstring(l, 1);
