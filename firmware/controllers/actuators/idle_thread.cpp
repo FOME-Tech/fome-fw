cff54ae7aa515806b96e08c38f34158e3f715cc0
Nathan Schulte <nmschulte@desmas.net>
2025-09-03 17:20:23 -0500

    idle A/C add: use isAcEnabled method
    it accounts for all things A/C actuation, post switch demand (but pre-delay)

--- firmware/controllers/actuators/idle_thread.cpp
+++ firmware/controllers/actuators/idle_thread.cpp
@@ -30,8 +30,7 @@ IIdleController::TargetInfo IdleController::getTargetRpm(float clt) {
 	// Why do we bump based on button not based on actual A/C relay state?
 	// Because AC output has a delay to allow idle bump to happen first, so that the airflow increase gets a head start on the load increase
 	// alternator duty cycle has a similar logic
-	targetRpmAcBump = (!hasAcPressure() || engine->module<AcController>().unmock().acPressureSwitchState)
-		&& engine->module<AcController>().unmock().acButtonState ? engineConfiguration->acIdleRpmBump : 0;
+	targetRpmAcBump = engine->module<AcController>().unmock().isAcEnabled() ? engineConfiguration->acIdleRpmBump : 0;
 
 	auto target = targetRpmByClt + targetRpmAcBump + luaAddRpm;
 
@@ -131,8 +130,7 @@ percent_t IdleController::getRunningOpenLoop(float rpm, float clt, SensorResult 
 	openLoopBase = running;
 
 	// Now we bump it by the AC/fan amount if necessary
-	openLoopAcBump = (!hasAcPressure() || engine->module<AcController>().unmock().acPressureSwitchState)
-		&& engine->module<AcController>().unmock().acButtonState ? engineConfiguration->acIdleExtraOffset : 0;
+	openLoopAcBump = engine->module<AcController>().unmock().isAcEnabled() ? engineConfiguration->acIdleExtraOffset : 0;
 	openLoopFanBump =
 		  (enginePins.fanRelay.getLogicValue()  ? engineConfiguration->fan1ExtraIdle : 0)
 		+ (enginePins.fanRelay2.getLogicValue() ? engineConfiguration->fan2ExtraIdle : 0);
