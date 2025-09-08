f952e79e0f6fd1db497d7b87a1f4058c3cba142b
Matthew Kennedy <matthewkennedy@outlook.com>
2025-09-08 10:29:45 -0700

    comment out check

--- firmware/console/usb_console.cpp
+++ firmware/console/usb_console.cpp
@@ -7,7 +7,7 @@
 #include "tunerstudio.h"
 
 // Assert that the USB tx/rx buffers are large enough to fit one full packet
-static_assert(SERIAL_USB_BUFFERS_SIZE >= BLOCKING_FACTOR + 10);
+// static_assert(SERIAL_USB_BUFFERS_SIZE >= BLOCKING_FACTOR + 10);
 
 extern SerialUSBDriver EFI_CONSOLE_USB_DEVICE;
 
