3897f8814031fbddf955884d6f4363c62ce23fa5
Creesic <38671556+Creesic@users.noreply.github.com>
2025-10-01 20:11:31 -0400

    Update trigger_bmw.cpp

--- firmware/controllers/trigger/decoders/trigger_bmw.cpp
+++ firmware/controllers/trigger/decoders/trigger_bmw.cpp
@@ -6,29 +6,18 @@
 
 void initializeVvtN63TU(TriggerWaveform *s) {
     s->initialize(FOUR_STROKE_CAM_SENSOR, SyncEdge::Both);
-    // True means it's a cam wheel
-    s->isSecondWheelCam = true;
+    
+    s->shapeWithoutTdc = true;
 
-    // per-state ratio acceptance windows
-    s->setTriggerSynchronizationGap3(0, 1.8f, 3.2f);   // around 2.60
-    s->setTriggerSynchronizationGap3(1, 1.10f, 1.80f); // around 1.385
-    s->setTriggerSynchronizationGap3(2, 0.15f, 0.35f); // around 0.222
-    s->setTriggerSynchronizationGap3(3, 2.5f, 4.5f);   // around 3.50
-    s->setTriggerSynchronizationGap3(4, 1.05f, 1.65f); // around 1.286
-    s->setTriggerSynchronizationGap3(5, 0.20f, 0.50f); // around 0.278
+	s->setTriggerSynchronizationGap(0.581);
+	s->setSecondTriggerSynchronizationGap(1.722);
 
-    //r0 = g0/g5 = 65/25 = 2.600000  Gap ratio calculation
-    //r1 = g1/g0 = 90/65 = 1.384615
-    //r2 = g2/g1 = 20/90 = 0.222222
-    //r3 = g3/g2 = 70/20 = 3.500000
-    //r4 = g4/g3 = 90/70 = 1.285714
-    //r5 = g5/g4 = 25/90 = 0.277778
-
-    // Edges: (angle, isRising, wheel)
-    s->addEvent720( 50.0f,  true,  TriggerWheel::T_PRIMARY);
-    s->addEvent720(180.0f,  false, TriggerWheel::T_PRIMARY);
-    s->addEvent720(360.0f,  true,  TriggerWheel::T_PRIMARY);
-    s->addEvent720(400.0f,  false, TriggerWheel::T_PRIMARY);
-    s->addEvent720(540.0f,  true,  TriggerWheel::T_PRIMARY);
-    s->addEvent720(720.0f,  false, TriggerWheel::T_PRIMARY);
+    s->addEvent360(25, TriggerValue::RISE); //pos1
+    s->addEvent360(90, TriggerValue::FALL); //pos2
+    
+    s->addEvent360(180, TriggerValue::RISE); //pos3
+    s->addEvent360(200, TriggerValue::FALL); //pos4
+    
+    s->addEvent360(270, TriggerValue::RISE); //pos5
+    s->addEvent360(360, TriggerValue::FALL); //pos6
 }
