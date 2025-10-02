4818b71ef0b9512978f2f7757dd9da8d6e4af014
Creesic <38671556+Creesic@users.noreply.github.com>
2025-10-01 22:52:45 -0400

    Update trigger_bmw.cpp

--- firmware/controllers/trigger/decoders/trigger_bmw.cpp
+++ firmware/controllers/trigger/decoders/trigger_bmw.cpp
@@ -9,15 +9,15 @@ void initializeVvtN63TU(TriggerWaveform *s) {
     
     s->shapeWithoutTdc = true;
 
-	s->setTriggerSynchronizationGap2(0.70f, 0.82f);
-	s->setSecondTriggerSynchronizationGap2(1.25f, 1.40f);
+	s->setTriggerSynchronizationGap2(0.56f, 0.70f);
+	s->setSecondTriggerSynchronizationGap2(1.28f, 1.62f);
 
-    s->addEvent360(25, false, TriggerWheel::T_PRIMARY);
-    s->addEvent360(90, true, TriggerWheel::T_PRIMARY);
+    s->addEvent360(25, true, TriggerWheel::T_PRIMARY);
+    s->addEvent360(90, false, TriggerWheel::T_PRIMARY);
 
-    s->addEvent360(180, false, TriggerWheel::T_PRIMARY);
-    s->addEvent360(200, true, TriggerWheel::T_PRIMARY);
+    s->addEvent360(180, true, TriggerWheel::T_PRIMARY);
+    s->addEvent360(200, false, TriggerWheel::T_PRIMARY);
     
-    s->addEvent360(270, false, TriggerWheel::T_PRIMARY);
-    s->addEvent360(360, true, TriggerWheel::T_PRIMARY);
+    s->addEvent360(270, true, TriggerWheel::T_PRIMARY);
+    s->addEvent360(360, false, TriggerWheel::T_PRIMARY);
 }
