c5e80d165b4bbded946b604651019d2ce1bd6089
Creesic <38671556+Creesic@users.noreply.github.com>
2025-10-02 17:13:16 -0400

    Update trigger_bmw.cpp

--- firmware/controllers/trigger/decoders/trigger_bmw.cpp
+++ firmware/controllers/trigger/decoders/trigger_bmw.cpp
@@ -5,11 +5,12 @@
 #include "trigger_bmw.h"
 
 void initializeVvtN63TU(TriggerWaveform *s) {
-    s->initialize(FOUR_STROKE_CAM_SENSOR, SyncEdge::Fall);
+    s->initialize(FOUR_STROKE_CAM_SENSOR, SyncEdge::Both);
     
     s->shapeWithoutTdc = true;
 
-	s->setTriggerSynchronizationGap2(1.4, 1.6);
+	s->setTriggerSynchronizationGap2(0.6f, 0.8f);
+	s->setSecondTriggerSynchronizationGap2(1.4f, 1.6f);
 
     s->addEvent360(65, true, TriggerWheel::T_PRIMARY);
     s->addEvent360(150, false, TriggerWheel::T_PRIMARY);
