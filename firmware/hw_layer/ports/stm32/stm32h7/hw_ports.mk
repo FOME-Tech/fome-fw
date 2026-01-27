bf02a7b40dc503c3586ccfc3d4caf7d8404dc4ab
Matthew Kennedy <matthewkennedy@outlook.com>
2026-01-27 15:00:27 -0800

    H7 uses board.c not from ChibiOS

--- firmware/hw_layer/ports/stm32/stm32h7/hw_ports.mk
+++ firmware/hw_layer/ports/stm32/stm32h7/hw_ports.mk
@@ -17,5 +17,5 @@ MCU = cortex-m7
 USE_FPU = hard
 USE_FPU_OPT = -mfloat-abi=$(USE_FPU) -mfpu=fpv5-d16
 LDSCRIPT = $(PROJECT_DIR)/hw_layer/ports/stm32/stm32h7/STM32H743xI.ld
-ALLCSRC += $(CHIBIOS)/os/hal/boards/ST_NUCLEO144_H743ZI/board.c
+ALLCSRC += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32h7/board.c
 CONFDIR = $(PROJECT_DIR)/hw_layer/ports/stm32/stm32h7/cfg
