681655c78eff238ae3eb8701a19941823feb01ca
Matthew Kennedy <matthewkennedy@outlook.com>
2026-01-28 00:25:32 -0800

    SDC config changes

--- firmware/hw_layer/mmc_card_attach.cpp
+++ firmware/hw_layer/mmc_card_attach.cpp
@@ -116,7 +116,8 @@ void stopMmcBlockDevice() {
 // Some ECUs are wired for SDIO/SDMMC instead of SPI
 #ifdef EFI_SDC_DEVICE
 static const SDCConfig sdcConfig = {
-	SDC_MODE_4BIT
+	.bus_width = SDC_MODE_4BIT,
+	.slowdown = 0,
 };
 
 BaseBlockDevice* initializeMmcBlockDevice() {
