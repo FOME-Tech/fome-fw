31ce9c7ccaa1ecd87dd28a910b60709bf766d604
Matthew Kennedy <matthewkennedy@outlook.com>
2026-02-02 12:06:36 -0800

    fan test cleanup

--- unit_tests/tests/actuators/test_fan_control.cpp
+++ unit_tests/tests/actuators/test_fan_control.cpp
@@ -1,17 +1,18 @@
 #include "pch.h"
 
-static void updateFans() {
+static void updateFan1() {
 	engine->module<FanControl1>()->onSlowCallback();
 }
 
-TEST(Actuators, Fan) {
-	struct MockAc : public AcController {
-		bool acState = false;
+struct MockAc : public AcController {
+	bool acState = false;
 
-		bool isAcEnabled() const override {
-			return acState;
-		}
-	};
+	bool isAcEnabled() const override {
+		return acState;
+	}
+};
+
+TEST(Actuators, Fan) {
 
 	EngineTestHelper eth(engine_type_e::TEST_ENGINE);
 	MockAc mockAc;
@@ -26,72 +27,72 @@ TEST(Actuators, Fan) {
 
 	// Cold, fan should be off
 	Sensor::setMockValue(SensorType::Clt, 75);
-	updateFans();
+	updateFan1();
 	EXPECT_EQ(false, enginePins.fanRelay.getLogicValue());
 
 	// Between thresholds, should still be off
 	Sensor::setMockValue(SensorType::Clt, 85);
-	updateFans();
+	updateFan1();
 	EXPECT_EQ(false, enginePins.fanRelay.getLogicValue());
 
 	// Hot, fan should turn on
 	Sensor::setMockValue(SensorType::Clt, 95);
-	updateFans();
+	updateFan1();
 	EXPECT_EQ(true, enginePins.fanRelay.getLogicValue());
 
 	// Between thresholds, should stay on
 	Sensor::setMockValue(SensorType::Clt, 85);
-	updateFans();
+	updateFan1();
 	EXPECT_EQ(true, enginePins.fanRelay.getLogicValue());
 
 	// Below threshold, should turn off
 	Sensor::setMockValue(SensorType::Clt, 75);
-	updateFans();
+	updateFan1();
 	EXPECT_EQ(false, enginePins.fanRelay.getLogicValue());
 
 	// Break the CLT sensor - fan turns on
 	Sensor::setInvalidMockValue(SensorType::Clt);
-	updateFans();
+	updateFan1();
 	EXPECT_EQ(true, enginePins.fanRelay.getLogicValue());
 
 	// CLT sensor back to normal, fan turns off
 	Sensor::setMockValue(SensorType::Clt, 75);
-	updateFans();
+	updateFan1();
 	EXPECT_EQ(false, enginePins.fanRelay.getLogicValue());
 
 	engineConfiguration->enableFan1WithAc = true;
 	// Now AC is on, fan should turn on!
 	mockAc.acState = true;
-	updateFans();
+	updateFan1();
 	EXPECT_EQ(true, enginePins.fanRelay.getLogicValue());
 
 	// Turn off AC, fan should turn off too.
 	mockAc.acState = false;
-	updateFans();
+	updateFan1();
 	EXPECT_EQ(false, enginePins.fanRelay.getLogicValue());
 
 	// Back to hot, fan should turn on
 	Sensor::setMockValue(SensorType::Clt, 95);
-	updateFans();
+	updateFan1();
 	EXPECT_EQ(true, enginePins.fanRelay.getLogicValue());
 
 	// Engine starts cranking, fan should turn off
 	engine->rpmCalculator.setRpmValue(100);
-	updateFans();
+	updateFan1();
 	EXPECT_EQ(false, enginePins.fanRelay.getLogicValue());
 
 	// Engine running, fan should turn back on
 	engine->rpmCalculator.setRpmValue(1000);
-	updateFans();
+	updateFan1();
 	EXPECT_EQ(true, enginePins.fanRelay.getLogicValue());
 
 	// Stop the engine, fan should stay on
 	engine->rpmCalculator.setRpmValue(0);
-	updateFans();
+	updateFan1();
 	EXPECT_EQ(true, enginePins.fanRelay.getLogicValue());
 
 	// Set configuration to inhibit fan while engine is stopped, fan should stop
 	engineConfiguration->disableFan1WhenStopped = true;
-	updateFans();
+	updateFan1();
 	EXPECT_EQ(false, enginePins.fanRelay.getLogicValue());
 }
