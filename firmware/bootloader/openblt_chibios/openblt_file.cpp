78266f06b3c8b21dc20855e0588380f837b58d34
Matthew Kennedy <matthewkennedy@outlook.com>
2026-02-09 00:40:54 -0800

    format openblt_chibios

--- firmware/bootloader/openblt_chibios/openblt_file.cpp
+++ firmware/bootloader/openblt_chibios/openblt_file.cpp
@@ -9,22 +9,20 @@ extern "C" {
 
 static const blt_char firmwareFilename[] = "/fome_update.srec";
 
-static struct
-{
-	FIL      handle;                  /**< FatFS handle to the log-file.                 */
-	blt_bool canUse;                  /**< Flag to indicate if the log-file can be used. */
+static struct {
+	FIL handle;		 /**< FatFS handle to the log-file.                 */
+	blt_bool canUse; /**< Flag to indicate if the log-file can be used. */
 } logfile;
 
-/************************************************************************************//**
-** \brief     Callback that gets called to check whether a firmware update from 
+/*
+** \brief     Callback that gets called to check whether a firmware update from
 **            local file storage should be started. This could for example be when
-**            a switch is pressed, when a certain file is found on the local file 
+**            a switch is pressed, when a certain file is found on the local file
 **            storage, etc.
 ** \return    BLT_TRUE if a firmware update is requested, BLT_FALSE otherwise.
 **
 ****************************************************************************************/
-extern "C" blt_bool FileIsFirmwareUpdateRequestedHook()
-{
+extern "C" blt_bool FileIsFirmwareUpdateRequestedHook() {
 	// First, try to attach the SD card
 	if (!initializeMmcBlockDevice()) {
 		// No SD card present, no update to be done
@@ -53,27 +51,25 @@ extern "C" blt_bool FileIsFirmwareUpdateRequestedHook()
 	return BLT_FALSE;
 }
 
-/************************************************************************************//**
+/*
 ** \brief     Callback to obtain the filename of the firmware file that should be
-**            used during the firmware update from the local file storage. This 
+**            used during the firmware update from the local file storage. This
 **            hook function is called at the beginning of the firmware update from
-**            local storage sequence. 
+**            local storage sequence.
 ** \return    valid firmware filename with full path or BLT_NULL.
 **
 ****************************************************************************************/
-extern "C" const blt_char *FileGetFirmwareFilenameHook()
-{
+extern "C" const blt_char* FileGetFirmwareFilenameHook() {
 	return firmwareFilename;
 }
 
-/************************************************************************************//**
+/*
 ** \brief     Callback that gets called to inform the application that a firmware
 **            update from local storage just started.
 ** \return    none.
 **
 ****************************************************************************************/
-extern "C" void FileFirmwareUpdateStartedHook()
-{
+extern "C" void FileFirmwareUpdateStartedHook() {
 	/* create/overwrite the logfile */
 	logfile.canUse = BLT_FALSE;
 	if (f_open(&logfile.handle, "/bootlog.txt", FA_CREATE_ALWAYS | FA_WRITE) == FR_OK) {
@@ -91,29 +87,27 @@ extern "C" void FileFirmwareUpdateCompletedHook(void) {
 	f_unlink(firmwareFilename);
 }
 
-/************************************************************************************//**
+/*
 ** \brief     Callback that gets called in case an error occurred during a firmware
 **            update. Refer to <file.h> for a list of available error codes.
 ** \return    none.
 **
 ****************************************************************************************/
-extern "C" void FileFirmwareUpdateErrorHook(blt_int8u error_code)
-{
+extern "C" void FileFirmwareUpdateErrorHook(blt_int8u error_code) {
 	/* error detected which stops the firmware update, so close the log file */
 	if (logfile.canUse == BLT_TRUE) {
 		f_close(&logfile.handle);
 	}
 }
 
-/************************************************************************************//**
-** \brief     Callback that gets called each time new log information becomes 
+/*
+** \brief     Callback that gets called each time new log information becomes
 **            available during a firmware update.
 ** \param     info_string Pointer to a character array with the log entry info.
 ** \return    none.
 **
 ****************************************************************************************/
-extern "C" void FileFirmwareUpdateLogHook(blt_char *info_string)
-{
+extern "C" void FileFirmwareUpdateLogHook(blt_char* info_string) {
 	/* write the string to the log file */
 	if (logfile.canUse == BLT_TRUE) {
 		if (f_puts(info_string, &logfile.handle) < 0) {
