8ed8d8bdec85226351506bab5bbf5e087eb22de4
GitHub gen-configs Action <action@github.com>
2026-03-14 10:36:40 +0000

    Auto-generated configs and docs

--- firmware/controllers/trigger/trigger_state_generated.h
+++ firmware/controllers/trigger/trigger_state_generated.h
@@ -6,26 +6,28 @@ struct trigger_state_s {
 	float triggerSyncGapRatio = (float)0;
 	// Sync counter
 	// offset 4
-	uint32_t crankSynchronizationCounter = (uint32_t)0;
+	uint16_t crankSynchronizationCounter = (uint16_t)0;
 	// Sync error counter
-	// offset 8
+	// offset 6
 	uint16_t triggerErrorCounter = (uint16_t)0;
 	// Trigger state index
-	// offset 10
+	// offset 8
 	uint8_t triggerStateIndex = (uint8_t)0;
-	// offset 11
-	uint8_t alignmentFill_at_11[1];
+	// offset 9
+	uint8_t alignmentFill_at_9[1];
 	// Edge count rise
-	// offset 12
+	// offset 10
 	uint16_t edgeCountRise = (uint16_t)0;
 	// Edge count fall
-	// offset 14
+	// offset 12
 	uint16_t edgeCountFall = (uint16_t)0;
+	// offset 14
+	uint8_t alignmentFill_at_14[2];
 };
 static_assert(sizeof(trigger_state_s) == 16);
 static_assert(offsetof(trigger_state_s, triggerSyncGapRatio) == 0);
 static_assert(offsetof(trigger_state_s, crankSynchronizationCounter) == 4);
-static_assert(offsetof(trigger_state_s, triggerErrorCounter) == 8);
-static_assert(offsetof(trigger_state_s, triggerStateIndex) == 10);
-static_assert(offsetof(trigger_state_s, edgeCountRise) == 12);
-static_assert(offsetof(trigger_state_s, edgeCountFall) == 14);
+static_assert(offsetof(trigger_state_s, triggerErrorCounter) == 6);
+static_assert(offsetof(trigger_state_s, triggerStateIndex) == 8);
+static_assert(offsetof(trigger_state_s, edgeCountRise) == 10);
+static_assert(offsetof(trigger_state_s, edgeCountFall) == 12);
