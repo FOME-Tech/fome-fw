cf20585a6046465a9c37bd3b3e9fb73d60dc44e4
Matthew Kennedy <matthewkennedy@outlook.com>
2026-05-06 21:54:01 -0700

    dead stuff

--- unit_tests/boards.cpp
+++ unit_tests/boards.cpp
@@ -10,18 +10,6 @@
 #include "boards.h"
 #include "backup_ram.h"
 
-float getVoltageDivided(const char* msg, adc_channel_e hwChannel) {
-	return 0;
-}
-
-float getVoltage(const char* msg, adc_channel_e hwChannel) {
-	return 0;
-}
-
-int getAdcValue(const char* msg, adc_channel_e hwChannel) {
-	return 0;
-}
-
 BackupSramData* getBackupSram() {
 	static BackupSramData data;
 	return &data;
--- unit_tests/boards.h
+++ unit_tests/boards.h
@@ -8,9 +8,6 @@
 #pragma once
 
 #include "engine_configuration.h"
-#include "adc_math.h"
-
-#define ADC_CHANNEL_VREF 0
 
 int getAdcValue(const char* msg, adc_channel_e channel);
 
