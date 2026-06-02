ec394587039e2e6e420fb46a114e7f7e38ded024
GitHub gen-configs Action <action@github.com>
2026-06-02 01:49:35 +0000

    Auto-generated configs and docs

--- firmware/controllers/algo/torque/torque_model_generated.h
+++ firmware/controllers/algo/torque/torque_model_generated.h
@@ -17,100 +17,106 @@ struct torque_model_s {
 	// Nm
 	// offset 12
 	float m_torqueLoss = (float)0;
-	// Requested torque (limited)
-	// Nm
+	// Loss table Y axis value
 	// offset 16
+	int16_t m_torqueLossLoadAxisValue = (int16_t)0;
+	// offset 18
+	uint8_t alignmentFill_at_18[2];
+	// Gross torque
+	// Nm
+	// offset 20
 	float m_grossTorque = (float)0;
 	// Target cycle airmass
 	// g
-	// offset 20
+	// offset 24
 	float m_airmassTarget = (float)0;
 	// Measured cycle airmass
 	// g
-	// offset 24
+	// offset 28
 	float m_airmassActual = (float)0;
 	// Target airmass CL trim
 	// %
-	// offset 28
+	// offset 32
 	float m_airmassTrim = (float)0;
 	// Requested throttle position
 	// %
-	// offset 32
+	// offset 36
 	float m_throttleRequest = (float)0;
 	// Limited by engine max
-	// offset 36 bit 0
+	// offset 40 bit 0
 	bool limitedByEngineMax : 1 {};
 	// Limited by axle max
-	// offset 36 bit 1
+	// offset 40 bit 1
 	bool limitedByAxleMax : 1 {};
-	// offset 36 bit 2
-	bool unusedBit_36_2 : 1 {};
-	// offset 36 bit 3
-	bool unusedBit_36_3 : 1 {};
-	// offset 36 bit 4
-	bool unusedBit_36_4 : 1 {};
-	// offset 36 bit 5
-	bool unusedBit_36_5 : 1 {};
-	// offset 36 bit 6
-	bool unusedBit_36_6 : 1 {};
-	// offset 36 bit 7
-	bool unusedBit_36_7 : 1 {};
-	// offset 36 bit 8
-	bool unusedBit_36_8 : 1 {};
-	// offset 36 bit 9
-	bool unusedBit_36_9 : 1 {};
-	// offset 36 bit 10
-	bool unusedBit_36_10 : 1 {};
-	// offset 36 bit 11
-	bool unusedBit_36_11 : 1 {};
-	// offset 36 bit 12
-	bool unusedBit_36_12 : 1 {};
-	// offset 36 bit 13
-	bool unusedBit_36_13 : 1 {};
-	// offset 36 bit 14
-	bool unusedBit_36_14 : 1 {};
-	// offset 36 bit 15
-	bool unusedBit_36_15 : 1 {};
-	// offset 36 bit 16
-	bool unusedBit_36_16 : 1 {};
-	// offset 36 bit 17
-	bool unusedBit_36_17 : 1 {};
-	// offset 36 bit 18
-	bool unusedBit_36_18 : 1 {};
-	// offset 36 bit 19
-	bool unusedBit_36_19 : 1 {};
-	// offset 36 bit 20
-	bool unusedBit_36_20 : 1 {};
-	// offset 36 bit 21
-	bool unusedBit_36_21 : 1 {};
-	// offset 36 bit 22
-	bool unusedBit_36_22 : 1 {};
-	// offset 36 bit 23
-	bool unusedBit_36_23 : 1 {};
-	// offset 36 bit 24
-	bool unusedBit_36_24 : 1 {};
-	// offset 36 bit 25
-	bool unusedBit_36_25 : 1 {};
-	// offset 36 bit 26
-	bool unusedBit_36_26 : 1 {};
-	// offset 36 bit 27
-	bool unusedBit_36_27 : 1 {};
-	// offset 36 bit 28
-	bool unusedBit_36_28 : 1 {};
-	// offset 36 bit 29
-	bool unusedBit_36_29 : 1 {};
-	// offset 36 bit 30
-	bool unusedBit_36_30 : 1 {};
-	// offset 36 bit 31
-	bool unusedBit_36_31 : 1 {};
+	// offset 40 bit 2
+	bool unusedBit_40_2 : 1 {};
+	// offset 40 bit 3
+	bool unusedBit_40_3 : 1 {};
+	// offset 40 bit 4
+	bool unusedBit_40_4 : 1 {};
+	// offset 40 bit 5
+	bool unusedBit_40_5 : 1 {};
+	// offset 40 bit 6
+	bool unusedBit_40_6 : 1 {};
+	// offset 40 bit 7
+	bool unusedBit_40_7 : 1 {};
+	// offset 40 bit 8
+	bool unusedBit_40_8 : 1 {};
+	// offset 40 bit 9
+	bool unusedBit_40_9 : 1 {};
+	// offset 40 bit 10
+	bool unusedBit_40_10 : 1 {};
+	// offset 40 bit 11
+	bool unusedBit_40_11 : 1 {};
+	// offset 40 bit 12
+	bool unusedBit_40_12 : 1 {};
+	// offset 40 bit 13
+	bool unusedBit_40_13 : 1 {};
+	// offset 40 bit 14
+	bool unusedBit_40_14 : 1 {};
+	// offset 40 bit 15
+	bool unusedBit_40_15 : 1 {};
+	// offset 40 bit 16
+	bool unusedBit_40_16 : 1 {};
+	// offset 40 bit 17
+	bool unusedBit_40_17 : 1 {};
+	// offset 40 bit 18
+	bool unusedBit_40_18 : 1 {};
+	// offset 40 bit 19
+	bool unusedBit_40_19 : 1 {};
+	// offset 40 bit 20
+	bool unusedBit_40_20 : 1 {};
+	// offset 40 bit 21
+	bool unusedBit_40_21 : 1 {};
+	// offset 40 bit 22
+	bool unusedBit_40_22 : 1 {};
+	// offset 40 bit 23
+	bool unusedBit_40_23 : 1 {};
+	// offset 40 bit 24
+	bool unusedBit_40_24 : 1 {};
+	// offset 40 bit 25
+	bool unusedBit_40_25 : 1 {};
+	// offset 40 bit 26
+	bool unusedBit_40_26 : 1 {};
+	// offset 40 bit 27
+	bool unusedBit_40_27 : 1 {};
+	// offset 40 bit 28
+	bool unusedBit_40_28 : 1 {};
+	// offset 40 bit 29
+	bool unusedBit_40_29 : 1 {};
+	// offset 40 bit 30
+	bool unusedBit_40_30 : 1 {};
+	// offset 40 bit 31
+	bool unusedBit_40_31 : 1 {};
 };
-static_assert(sizeof(torque_model_s) == 40);
+static_assert(sizeof(torque_model_s) == 44);
 static_assert(offsetof(torque_model_s, m_driverTorqueDemand) == 0);
 static_assert(offsetof(torque_model_s, m_torqueRequested) == 4);
 static_assert(offsetof(torque_model_s, m_torqueRequestedLimited) == 8);
 static_assert(offsetof(torque_model_s, m_torqueLoss) == 12);
-static_assert(offsetof(torque_model_s, m_grossTorque) == 16);
-static_assert(offsetof(torque_model_s, m_airmassTarget) == 20);
-static_assert(offsetof(torque_model_s, m_airmassActual) == 24);
-static_assert(offsetof(torque_model_s, m_airmassTrim) == 28);
-static_assert(offsetof(torque_model_s, m_throttleRequest) == 32);
+static_assert(offsetof(torque_model_s, m_torqueLossLoadAxisValue) == 16);
+static_assert(offsetof(torque_model_s, m_grossTorque) == 20);
+static_assert(offsetof(torque_model_s, m_airmassTarget) == 24);
+static_assert(offsetof(torque_model_s, m_airmassActual) == 28);
+static_assert(offsetof(torque_model_s, m_airmassTrim) == 32);
+static_assert(offsetof(torque_model_s, m_throttleRequest) == 36);
