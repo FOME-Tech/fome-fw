a2aff6a577af423e3b7319f3200ca3506c854b6b
Matthew Kennedy <matthewkennedy@outlook.com>
2026-06-16 01:57:04 -0700

    double header

--- firmware/console/binary/tunerstudio.cpp
+++ firmware/console/binary/tunerstudio.cpp
@@ -43,20 +43,6 @@
  * You should have received a copy of the GNU General Public License along with this program.
  * If not, see <http://www.gnu.org/licenses/>.
  *
- *
- * This file is part of rusEfi - see http://rusefi.com
- *
- * rusEfi is free software; you can redistribute it and/or modify it under the terms of
- * the GNU General Public License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * rusEfi is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with this program.
- * If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
 #include "pch.h"
@@ -69,7 +55,6 @@
 
 #include "tunerstudio_io.h"
 #include "console_io.h"
-#include "tunerstudio_io.h"
 #include "trigger_scope.h"
 #include "electronic_throttle.h"
 #include "live_data.h"
