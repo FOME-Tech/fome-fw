0ffa5807b297e4e3adbd784d72a8d18996f16672
Matthew Kennedy <matthewkennedy@outlook.com>
2026-08-28 12:26:19 -0700

    missed a spot

--- firmware/console/binary_log/binary_logging.h
+++ firmware/console/binary_log/binary_logging.h
@@ -3,8 +3,12 @@
  */
 
 #include <cstddef>
+#include <cstdint>
 
 struct Writer;
 void writeFileHeader(Writer& buffer);
 void writeSdLogLine(Writer& buffer);
 void writeSdBlock(Writer& outBuffer);
+
+// Per-block overhead: block type, rolling counter, 2 byte timestamp, 1 byte checksum
+#define MLQ_BLOCK_OVERHEAD 5
