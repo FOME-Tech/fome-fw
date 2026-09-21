8b3079c8d8a3a76d10a8f6429af2043055fefe58
Matthew Kennedy <matthewkennedy@outlook.com>
2026-09-21 15:34:51 -0700

    fix timer negative delta handling

--- unit_tests/tests/util/test_timer.cpp
+++ unit_tests/tests/util/test_timer.cpp
@@ -11,3 +11,17 @@ TEST(util, timer) {
 	eth.moveTimeForwardSec(4);
 	ASSERT_TRUE(timer.hasElapsedSec(3));
 }
+
+TEST(util, timerResetInFuture) {
+	EngineTestHelper eth(engine_type_e::TEST_ENGINE);
+	Timer timer;
+
+	// Simulate an interrupt resetting the timer between the reader sampling
+	// the current time and reading the reset time: the reset is slightly in
+	// the "future" relative to now. That must not look like a huge elapsed time.
+	timer.reset(getTimeNowNt() + US2NT(10));
+
+	EXPECT_FALSE(timer.hasElapsedUs(1));
+	EXPECT_FALSE(timer.hasElapsedSec(2));
+	EXPECT_EQ(0, timer.getElapsedUs());
+}
