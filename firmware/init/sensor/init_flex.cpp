1a9f01880267216ef949c3fc41405af0ee2edd1a
Matthew Kennedy <matthewkennedy@outlook.com>
2026-09-21 18:52:23 -0700

    simplify init_flex

--- firmware/init/sensor/init_flex.cpp
+++ firmware/init/sensor/init_flex.cpp
@@ -15,7 +15,7 @@ static Biquad flexTempFilter;
 
 static Timer flexFreq, flexPulse;
 
-static void flexCallback(efitick_t nowNt, bool value) {
+static void flexCallback(void*, efitick_t nowNt, bool value) {
 	if (value) {
 		// End pulse timing on rising edge
 		float pulseWidthUs = flexPulse.getElapsedUs(nowNt);
@@ -41,10 +41,6 @@ static void flexCallback(efitick_t nowNt, bool value) {
 }
 
 static Gpio flexPin = Gpio::Unassigned;
-
-static void flexCallback(void*, efitick_t nowNt, bool value) {
-	flexCallback(nowNt, value);
-}
 
 // https://rusefi.com/forum/viewtopic.php?p=37452&sid=829804c90d5b2e1fecd1b900cf1b1811#p37452
 
